a105cc0ff79e22d17bf4f2841a48352b3ad5bf29
mohammed rishad <mohammedrishad17@gmail.com>
2022-04-23 19:38:25 +0400

    static home page created

---
+++ app/javascript/packs/static/home.js
@@ -0,0 +1,9 @@
+import Vue from "vue/dist/vue.esm";
+import Home from "../../views/static/home.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    el: "#vue",
+    components: { Home },
+  });
+});
---
+++ app/javascript/views/static/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="home">
+    <p class="text-blue-500 text-center text-4xl mt-16">EkAnek Home Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+};
+</script>
+
+<style scoped></style>
